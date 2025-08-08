1bf210609a8f8e81d1c2b70cce04d5c500b86f11
Khush Jain <200010040@iitb.ac.in>
2025-08-08 20:39:45 +0530

    attendance: api creation

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
 from app.core.config import settings
 
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
 
 def init_db(session: Session) -> None:
     # Alembic migrations to be added here
