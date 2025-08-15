5573e9ed9f1da3a50c3e54e0b87189e4261e5532
Khush Jain <200010040@iitb.ac.in>
2025-08-15 18:44:05 +0530

    fix api router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from app.api.routes import dbtest
+from app.api.routes import dbtest, attendance, communities, events
 
 
 api_router = APIRouter()
 api_router.include_router(dbtest.router)
- 
+api_router.include_router(attendance.router)
+api_router.include_router(communities.router)
+api_router.include_router(events.router)
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.core.db import Base, engine
 from app.api.main import api_router
-from app.api.routes import attendance, communities, events
 
 
 @asynccontextmanager
@@ -15,9 +14,4 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
-
-
 app.include_router(api_router, prefix=settings.API_V1_STR)
-app.include_router(attendance.router, prefix=settings.API_V1_STR)
-app.include_router(communities.router, prefix=settings.API_V1_STR)
-app.include_router(events.router, prefix=settings.API_V1_STR)
